52bc78d036cd790fc1628bc10c501e895634fcc6
WerWolv <werwolv98@gmail.com>
2018-11-24 11:22:31 +0100

    Added bootloader code and a simple kernel program

---
+++ Kernel/Kernel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kernel
+{
+    public class Kernel
+    {
+        public int kernel_main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            return 0;
+        }
+
+        internal void test()
+        {
+            Console.WriteLine("NONONO");
+        }
+
+        private void test2()
+        {
+            Console.WriteLine("NONONO");
+        }
+    }
+}
---
+++ VirtualSpaceBoi/Bootloader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualSpaceBoi
+{
+    public class Bootloader
+    {
+        public static void Main(string[] args)
+        {
+            var kernelDll = Assembly.Load("Kernel");
+            // Init stuff
+            var type = kernelDll.GetType("Kernel.Kernel");
+            var method = type.GetMethod("kernel_main");
+            method.Invoke(Activator.CreateInstance(type), new object[] { args });
+            // Cleanup stuff
+
+            Console.WriteLine("Kernel died");
+            Console.ReadLine();
+        }
+    }
+}
